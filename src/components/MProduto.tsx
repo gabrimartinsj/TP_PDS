7c24fd009ed53fe59496181e87f246803f3ee79e
Denilson Martins Vieira Santos <denilsonmvs@gmail.com>
2022-10-21 21:50:48 -0300

    Parcial Carrinho

--- src/components/MProduto.tsx
+++ src/components/MProduto.tsx
@@ -1,12 +1,41 @@
 
-import { Produto } from "../@types/entities/Produto";
+import { View, StyleSheet, Image, Text } from "react-native";
 
+export type ProdutoCarrinho = {
+  imagem: string,
+  nome: string,
+  preco: number,
+  desconto: number,
+  quantidade: number,
+  idx: number
+};
 
 type MProdutoProps = {
-  produto: Produto,
-  quantity: number
+  produto: ProdutoCarrinho
 };
 
 export default function MProduto(props: MProdutoProps) {
-
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: "https://image-placeholder.com/images/actual-size/75x75.png"}} style={styles.image}></Image>
+      <View style={styles.imageInfoText}>
+        <Text>Teste</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 20
+  },
+  image: {
+    height: 200,
+    width: "auto",
+    resizeMode: "contain"
+  },
+  imageInfoText: {
+    flex: 1
+  }
+});
--- src/screens/Carrinho.tsx
+++ src/screens/Carrinho.tsx
@@ -2,13 +2,31 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Compra } from "../@types/entities/Compra";
 import ScreenContainer from "../components/ScreenContainer";
+import MProduto from "../components/MProduto";
+import { ProdutoCarrinho } from "../components/MProduto";
+
 
 type Props = {};
 
 const Carrinho = (props: Props) => {
+
+  const produtos: ProdutoCarrinho[] = [{
+    imagem: "https://image-placeholder.com/images/actual-size/75x75.png",
+    nome: "Calvin Clein Regular fit slim fit shirt",
+    preco: 52,
+    desconto: 20,
+    quantidade: 1,
+    idx: 0
+  }];
+
   return (
     <ScreenContainer>
       <Text>Carrinho</Text>
+      {produtos.map((produto, idx) => {
+        return (
+          <MProduto produto={produto} key={idx}/>
+        )
+      })}
     </ScreenContainer>
   );
 };
